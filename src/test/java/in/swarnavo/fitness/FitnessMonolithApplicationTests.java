94e84e6e733f9e216996e2f5212c2ef473e6fe2f
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-20 00:03:50 +0530

    first commit

---
+++ src/test/java/in/swarnavo/fitness/FitnessMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package in.swarnavo.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
