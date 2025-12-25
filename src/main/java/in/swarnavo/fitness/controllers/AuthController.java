2c84bce72d5aab64c0c48f5d279c33868884ec5a
Swarnavo2003 <swarnavo2020@gmail.com>
2025-12-25 13:29:34 +0530

    feat: request body input validation added along with custom excpetion handling

--- src/main/java/in/swarnavo/fitness/controllers/AuthController.java
+++ src/main/java/in/swarnavo/fitness/controllers/AuthController.java
@@ -5,6 +5,7 @@ import in.swarnavo.fitness.dtos.LoginResponse;
 import in.swarnavo.fitness.dtos.RegisterRequest;
 import in.swarnavo.fitness.dtos.UserResponse;
 import in.swarnavo.fitness.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +22,12 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
         return new ResponseEntity<>(userService.register(registerRequest), HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         LoginResponse loginResponse = userService.login(loginRequest);
         if(loginResponse == null) {
             return ResponseEntity.badRequest().build();
--- src/main/java/in/swarnavo/fitness/dtos/RegisterRequest.java
+++ src/main/java/in/swarnavo/fitness/dtos/RegisterRequest.java
@@ -1,5 +1,7 @@
 package in.swarnavo.fitness.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "FirstName is required")
     private String firstName;
+
+    @NotBlank(message = "LastName is required")
     private String lastName;
+
+    @NotBlank(message = "Role is required")
     private String role;
 }
